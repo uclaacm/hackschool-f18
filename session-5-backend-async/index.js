b22b56df5748c10deae57f1675701bd1005087af
Kristie Lim <kristie.lim@gmail.com>
2018-11-07 18:28:56 -0800

    Add files via upload

---
+++ session-5-backend-async/index.js
@@ -0,0 +1,16 @@
+const fetch = require('node-fetch');
+const express = require('express');
+const app = express();
+
+async function getJokeFromServer() {
+	const res = await fetch('http://api.icndb.com/jokes/random');
+	return res.json();
+}
+
+app.get('/', async (req, res) => {
+	const jsonFromServer = await getJokeFromServer();
+	const joke = jsonFromServer.value.joke;
+	res.send(joke);
+});
+
+app.listen(3000);
