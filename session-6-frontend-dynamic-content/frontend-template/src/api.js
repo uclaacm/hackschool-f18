abb89167ceb482f5e98c32c668d106d5fb096893
punteek <prateeksingh1@hotmail.com>
2018-11-14 17:37:17 -0800

    finished frontend-template

--- session-6-frontend-dynamic-content/frontend-template/src/api.js
+++ session-6-frontend-dynamic-content/frontend-template/src/api.js
@@ -16,43 +16,13 @@ const throwError = async (resp) => {
 };
 
 // TODO: Write a function that gets all of the posts from the database using a fetch
-const getPosts = async () => {
-    const resp = await fetch(APIURL);
-    if (!resp.ok) {
-        throwError(resp);
-    } else {
-        return resp.json();
-    }
-};
 
 // TODO: Write a function that adds a new post to the database using a fetch
-const addPost = async (post) => {
-    const resp = await fetch(APIURL, {
-        method: 'post',
-        headers: new Headers({
-            'Content-Type': 'application/json'
-        }),
-        body: JSON.stringify(post)
-    });
-    if (!resp.ok) {
-        throwError(resp);
-    }
-};
 
 // TODO: Write a function that deletes a post from the database using a fetch
-const deletePost = async (postId) => {
-    const delURL = APIURL + '/' + postId;
-    const resp = await fetch(delURL, {
-        method: 'delete'
-    });
-
-    if (!resp.ok) {
-        throwError(resp);
-    } 
-};
 
 export {
     //getPosts,
-    // addPost,
-    deletePost
+    addPost,
+    // deletePost
 };
